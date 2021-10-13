0f9ae3973f29e37added22c33956db42114ee2c3
hossamradwan <hossamxp33@gmail.com>
2021-10-12 19:00:20 -0700

    nn

--- src/services/cities/cities.service.ts
+++ src/services/cities/cities.service.ts
@@ -11,13 +11,8 @@ export class CitiesService {
   constructor(private http: HttpClient) { }
 
   add(data: any) {
-<<<<<<< HEAD
 
     return this.http.post(`${environment.endpoint}/Cities/add`, data,options).toPromise();
-=======
-    return this.http.post(`${environment.endpoint}/Cities/add`, data).toPromise();
 
-    //hossam s asd
->>>>>>> 6f5bb8cc1d68149b1fcbf1e29a251161b9e4c9c7
   }
 }
