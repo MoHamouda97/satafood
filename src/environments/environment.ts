34f41f2004f12b4c712578b25f62de5023930747
hossamxp33 <osama.omar712015@gmail.com>
2021-10-16 17:56:27 +0200

    addareacomponent

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  endpoint: 'http://localhost:3000/api',
+  endpoint: 'http://satafood.codesroots.com:3000/api',
   photoPath: 'http://localhost:3000'
 
 };
