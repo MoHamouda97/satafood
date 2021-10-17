1f52d1b45575829421647356603d2fd6e631434d
hossamxp33 <osama.omar712015@gmail.com>
2021-10-17 20:22:33 +0200

    categorieswork17/10/2021

--- src/services/categories/categories.service.ts
+++ src/services/categories/categories.service.ts
@@ -1,9 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { CategoriesModel } from 'src/app/models/MenuModel';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoriesService {
 
-  constructor() { }
+  
+
+  constructor(private http: HttpClient) { }
+
+
+  
+  GetArea() {
+
+    return this.http.get<Array<CategoriesModel>>(`${environment.endpoint}/Cities/getCities`).toPromise();
+
+  }
 }
+
